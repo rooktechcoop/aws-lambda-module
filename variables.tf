1beac0859f0cd969127c628d53ae1acc587de13f
Nicolas Cacik <nicocacik@gmail.com>
2022-06-14 16:44:01 -0300

    Modify the default value of environment

--- variables.tf
+++ variables.tf
@@ -29,7 +29,9 @@ variable "environment" {
   type = object({
     variables = map(string)
   })
-  default = {}
+  default = {
+    variables:{}
+  }
 }
 
 #Is the list of the files that are going to be source of the Lambda function.
